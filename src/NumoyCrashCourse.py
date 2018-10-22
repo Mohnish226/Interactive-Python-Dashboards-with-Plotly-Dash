e1c0085eeb7ea5b753cf583908a0acefe82371f3
Mohnish Devadiga <mohnish226@yahoo.co.in>
2018-10-22 22:13:26 +0530

    Numpy Crash Course

---
+++ src/NumoyCrashCourse.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+myList = [1,2,3,4]
+
+arr = np.array(myList)
+print(arr)
+print(type(arr))
+
+print(np.zeros((5,5)))
+
+print(np.ones((2,2)))
+
+print(np.random.randint(0,100))
+
+print(np.random.randint(0,100,(5,5)))
+
+print(np.random.seed(101))
+
+print(np.random.rand)
+
+print(np.linspace(0,10,6))
+
+print(np.linspace(0,10,102))
